ad77046c90f1581a2ecac09a1ebb490bff75fd1f
Julián Astudillo <julian12astudillo@gmail.com>
2022-08-05 19:27:40 -0300

    animations added

--- scripts/wordle.js
+++ scripts/wordle.js
@@ -1,8 +1,11 @@
 // Local json import
 import PALABRAS from "/palabras.json" assert { type: "json" };
 
-const guessGrid = document.querySelector("[data-guess-grid]");
 const WORD_LENGTH = 5;
+const targetWord = PALABRAS[Math.floor(Math.random() * PALABRAS.length)];
+console.log(targetWord);
+const guessGrid = document.querySelector("[data-guess-grid]");
+const alertContainer = document.querySelector("[data-alert-container]");
 
 startInteraction();
 
@@ -76,7 +79,7 @@ function handlePress(e) {
 
 /**
  * @param {*Key input from the handleClick and handlePress functions} key
- * This function add the input from the user to the "tile" divs
+ * This function adds the input from the user to the "tile" divs
  *
  * This function also stops the user from pressing
  * more keys when the amount of letter is equal
@@ -100,6 +103,60 @@ function deleteKey() {
   lastTile.textContent = "";
   delete lastTile.dataset.state;
   delete lastTile.dataset.letter;
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()];
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Not enough letters");
+    shakeTiles(activeTiles);
+    return;
+  }
+
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter;
+  }, "");
+
+  if (!PALABRAS.includes(guess)) {
+    showAlert("Not in word list");
+    shakeTiles(activeTiles);
+    return;
+  }
+
+  stopInteraction();
+  activeTiles.forEach((...params) => flipTiles(...params, guess));
+}
+
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div");
+  alert.textContent = message;
+  alert.classList.add("alert");
+  alertContainer.prepend(alert);
+  if (!duration) return;
+  setTimeout(() => {
+    alert.classList.add("hide");
+    alert.addEventListener("transitionend", () => {
+      alert.remove();
+    });
+  }, duration);
+}
+
+function flipTiles(tile, index, array, guess) {
+  const letter = tile.dataset.letter;
+  const key = keyboard.querySelector('[data-key="${letter}]');
+}
+
+function shakeTiles(tiles) {
+  tiles.forEach((tile) => {
+    tile.classList.add("shake");
+    tile.addEventListener(
+      "animationend",
+      () => {
+        tile.classList.remove("shake");
+      },
+      { once: true }
+    );
+  });
 }
 
 /**
